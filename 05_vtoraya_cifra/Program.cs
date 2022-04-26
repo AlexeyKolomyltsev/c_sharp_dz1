f9dc49ae093accfd364b1f63f3c7e396d745b169
AlexeyWatt <allexdoma2@gmail.com>
2022-04-26 14:33:11 +0300

    add 05 показать вторую цифру трехзначного числа

---
+++ 05_vtoraya_cifra/Program.cs
@@ -0,0 +1,8 @@
+﻿///Показать вторую цифру трёхзначного числа
+Console.Write("Put number a ");
+int a = int.Parse(Console.ReadLine());
+if (a > 99 &&  a < 1000)
+{
+    Console.Write("Вторая цифра введенного трехзначного числа " + a / 10 % 10);
+}
+else Console.Write("Введенное число не трехзначеое");
