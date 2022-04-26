f961ac3b3727eaf3ffd835a4ef2bf3173da1f757
AlexeyWatt <allexdoma2@gmail.com>
2022-04-26 19:16:26 +0300

    add 08 проверка кратности

---
+++ 08_kratnost/Program.cs
@@ -0,0 +1,15 @@
+﻿// Выяснить, кратно ли число заданному, если нет, вывести остаток.
+Console.Write("Введите число a ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите число b ");
+int b = int.Parse(Console.ReadLine());
+if (a < b)
+{
+    if (b % a == 0) Console.Write("Число b кратно a");
+    else Console.Write("Число b не кратно a, остаток " + b % a);
+}
+else
+{
+    if (a % b == 0) Console.Write("Число a кратно b");
+    else Console.Write("Число a не кратно b, остаток " + a % b);
+}
