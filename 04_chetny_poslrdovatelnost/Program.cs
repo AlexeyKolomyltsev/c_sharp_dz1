f8c8f052889832553655f625d4ee9486702c37e9
AlexeyWatt <allexdoma2@gmail.com>
2022-04-26 14:24:33 +0300

    add 04 последовательность четных чисел

---
+++ 04_chetny_poslrdovatelnost/Program.cs
@@ -0,0 +1,10 @@
+﻿// Показать четные числа от 1 до N
+Console.Write("Put number N ");
+int a = int.Parse(Console.ReadLine());
+int count = 2;
+Console.WriteLine($"Четные числа от 1 до {a}:");
+while (count <= a)
+{
+    Console.Write(count + " ");
+    count += 2;
+}
