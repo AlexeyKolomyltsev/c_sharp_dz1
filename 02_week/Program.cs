492b299d327db5241b1bc92306b7a8c61fd57521
AlexeyWatt <allexdoma2@gmail.com>
2022-04-26 14:07:38 +0300

    add 02 по номеру дня недели вывести его название

---
+++ 02_week/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.WriteLine("Ввести день недели");
+string day = Console.ReadLine();
+if(day == "1")
+{
+    Console.WriteLine(day + " день недели понедельник");
+}
+else if(day == "2")
+{
+    Console.WriteLine(day + " день недели вторник");
+}
+else if(day == "3")
+{
+    Console.WriteLine(day + " день недели среда");
+}
+else if(day == "4")
+{
+    Console.WriteLine(day + " день недели четверг");
+}
+else if(day == "5")
+{
+    Console.WriteLine(day + " день недели пятница");
+}
+else if(day == "6")
+{
+    Console.WriteLine(day + " день недели суббота");
+}
+else if(day == "7")
+{
+    Console.WriteLine(day + " день недели воскресенье");
+}
+else
+{
+    Console.WriteLine("Нет такого дня недели");
+}
