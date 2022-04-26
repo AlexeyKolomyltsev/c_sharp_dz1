2719e576e29c18e9abb8fb955ba99ecbfc26162a
AlexeyWatt <allexdoma2@gmail.com>
2022-04-26 18:40:40 +0300

    add 06 число из отрезка [10, 99]. Показать наибольшую цифру числа

---
+++ 06_chislo_0_99/Program.cs
@@ -0,0 +1,7 @@
+﻿///Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
+Console.Write("Put number a 10 - 99 ");
+int a = int.Parse(Console.ReadLine());
+int a1 = a / 10;
+int a2 = a % 10;
+if(a1 > a2) Console.Write(a1);
+else Console.Write(a2);
