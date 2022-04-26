a4f00259ae8cc87928196dfb8b64ae2954e5ab82
AlexeyWatt <allexdoma2@gmail.com>
2022-04-26 14:16:14 +0300

    add 03 проверка на четность

---
+++ 03_chetnost/Program.cs
@@ -0,0 +1,5 @@
+﻿// Выяснить является ли число чётным
+Console.Write("Put number a ");
+int a = int.Parse(Console.ReadLine());
+if(a % 2 == 0) Console.Write(a + " четное");
+else Console.Write(a + " нечетное");
